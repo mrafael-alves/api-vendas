18a206ae809d77c56d36966d931b06f575aee3b0
Rafael <rafael@example.com>
2023-08-30 11:41:33 -0300

    Add criptografia às senhas de usuário. Mudanças submetidas: modified:   src/modules/users/services/CreateUserService.ts

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../typeorm/repositories/UserRepository";
 import { User } from "../typeorm/entities/User";
+import { hash } from "bcryptjs";
 
 interface IRequest {
   name: string,
@@ -18,10 +19,12 @@ class CreateUserService {
       throw new AppError(400, 'Email address alreary used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     });
 
     await usersRepository.save(user);
