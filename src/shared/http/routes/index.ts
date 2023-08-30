74f1d7bfdbe0d26b6defe3fe8ac61d1a19436280
Rafael <rafael@example.com>
2023-08-30 12:19:56 -0300

    Cria controller e router para sessions. Mudanças submetidas: new file:   src/modules/users/controller/SessionsController.ts new file:   src/modules/users/routes/sessions.router.ts modified:   src/shared/http/routes/index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.router';
+import sessionsRouter from '@modules/users/routes/sessions.router';
 
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
