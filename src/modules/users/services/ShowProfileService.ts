4194fcb710c6c92f6e520cc015ebdf5b664d5215
Rafael <rafael@example.com>
2023-08-31 18:23:23 -0300

    Cria serviços show e update de user profile. Mudanças submetidas: new file:   src/modules/users/services/ShowProfileService.ts new file:   src/modules/users/services/UpdateProfileService

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,23 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../typeorm/repositories/UserRepository";
+import { User } from "../typeorm/entities/User";
+
+interface IRequest {
+  user_id: string
+}
+
+class ShowProfileService {
+  public async execute({user_id}: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const user = await usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError(400, 'Usernoit found');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
