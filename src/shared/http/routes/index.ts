723cede37e9e7584de6316e301b7c23b09fc1429
Rafael <rafael@example.com>
2023-08-31 18:49:47 -0300

    Cria rota para executar serviços de profile. Mudanças submetidas: new file:   src/modules/users/routes/profile.router.ts modified:   src/shared/http/routes/index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.router';
 import sessionsRouter from '@modules/users/routes/sessions.router';
 import passwordRouter from '@modules/users/routes/password.router';
+import profileRouter from '@modules/users/routes/profile.router';
 
 
 const routes = Router();
@@ -11,5 +12,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
