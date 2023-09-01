a1b6c64cd8a58687f26c3d40c7e4e7e48bceab08
Rafael <rafael@example.com>
2023-09-01 01:52:35 -0300

    Cria rotas para controlador de serviços de cliente Mudanças submetidas: new file:   src/modules/customers/routes/customers.routes.ts modified:   src/shared/http/routes/index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/users.router';
 import sessionsRouter from '@modules/users/routes/sessions.router';
 import passwordRouter from '@modules/users/routes/password.router';
 import profileRouter from '@modules/users/routes/profile.router';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 
 const routes = Router();
@@ -13,5 +14,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
