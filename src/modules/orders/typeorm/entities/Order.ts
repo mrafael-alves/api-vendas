8f337139ce1852aee9c27230f2bb67d8e7be9fad
Rafael <rafael@example.com>
2023-09-06 15:49:27 -0300

    Cria entity para tabela orders. Mudanças submetidas: new file:   src/modules/orders/typeorm/entities/Order.ts

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,19 @@
+import Customer from "@modules/customers/typeorm/entities/Customer";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('oreders')
+export default class Order{
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+}
